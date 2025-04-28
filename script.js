e270a569344bb9a2aa8b5e86f191120384e0674d
mireella <mirella.serafim@escola.pr.gov.br>
2025-04-28 20:28:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Mostrar botão de voltar ao topo
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+};
+
+topBtn.addEventListener("click", function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
 
